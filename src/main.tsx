fbb6d0cf0c158ee73721da260a6b1afa66b38d86
ZK Zhao <285744850+zkzzk34@users.noreply.github.com>
2026-09-15 11:43:02 -0400

    Build landing and consent components

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./browser/App";
 import "./styles/global.css";
+import "./styles/analytics-consent.css";
 import "./styles/recommendation-cards.css";
 import "./styles/recommendation-request.css";
 import "./styles/attribution.css";
